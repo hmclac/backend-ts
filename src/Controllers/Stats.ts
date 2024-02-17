eef95d092eaad0560e3bfa5e0cb8f9b1edf5d794
Kai <rieffelkai@gmail.com>
2024-02-17 03:01:28 -0800

    updates

--- src/Controllers/Stats.ts
+++ src/Controllers/Stats.ts
@@ -125,7 +125,9 @@ export class StatsController {
         .createQueryBuilder('swipe')
         .select('swipe.staff_name', 'staff_name')
         .addSelect('COUNT(*)', 'count')
-        .where('swipe.time_done > :oneMonthAgo', { oneMonthAgo: OneMonthAgo() })
+        .where('CAST(swipe.time_done) AS BIGINT > :oneMonthAgo', {
+          oneMonthAgo: OneMonthAgo()
+        })
         .groupBy('swipe.staff_name')
         .getRawMany();
 
